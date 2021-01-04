77d1b971d16083b46492ed707dee51c521f672df
HerySon <nicolas_rochet@hotmail.com>
2021-01-04 10:23:17 +0100

    add a linear regression

--- lekuto/trainer.py
+++ lekuto/trainer.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import StandartScaler
+from linear_model import LinearRegression
 
 class Trainer():
     def train():
         std = StandartScaler()
+        lm = LinearRegression()
